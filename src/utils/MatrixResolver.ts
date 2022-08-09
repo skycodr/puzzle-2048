8c5705959e495f1f0cd29cb0ffa5450d96e0f0a8
dulan-goexpedi <dulan.sudasinghe@goexpedi.com>
2022-08-10 01:49:47 +0530

    Coverting nested loops to a single loop in transpose

--- src/utils/MatrixResolver.ts
+++ src/utils/MatrixResolver.ts
@@ -1,17 +1,20 @@
 const MatrixResolver = (columns: number) => {
-  return {
-    l2m: (i: number) => [~~(i / columns), i % columns],
-    m2l: ([x, y]: Vector) => x * columns + y,
-    trn: (matrix: Matrix) => {
-      for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < i; j++) {
-          const tmp = matrix[i][j];
-          matrix[i][j] = matrix[j][i];
-          matrix[j][i] = tmp;
-        }
-      }
-    },
+  const l2m = (i: number) => [~~(i / columns), i % columns];
+  const m2l = ([x, y]: Vector) => x * columns + y;
+  const trn = (matrix: Matrix) => {
+    const l = matrix.length * matrix[0].length;
+
+    for (let k = 0; k < l; k++) {
+      const [i, j] = l2m(k);
+      if (i === j || j < i) continue;
+
+      const tmp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = tmp;
+    }
   };
+
+  return { l2m, m2l, trn };
 };
 
 export default MatrixResolver;
